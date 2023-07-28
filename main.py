34efb81aaf3a4f3c3f6f7396866e399a7d7214a4
Yaroslav5657 <129308847+Yaroslav5657@users.noreply.github.com>
2023-07-28 19:38:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Відкрити веб сторінку', web_app=WebAppInfo(url='https://www.mcdonalds.com/ua/uk-ua.html')))
+    markup.add(types.KeyboardButton('Відкрити веб сторінку', web_app=WebAppInfo(url='Сайт яким ви хочете вивести ')))
     await message.answer('Вітаю', reply_markup=markup)
 
 executor.start_polling(dp)
